292308259f95de90bbb05020d946cd08f935fe50
Adi Dargatsky <adidargatsky144@gmail.com>
2025-06-28 13:56:41 +0300

    Find Components By Name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,10 +6,30 @@ public class Main {
         reader.chooseFile();
         try {
             reader.read();
-            System.out.println(reader.convertToMetadata());
+            Metadata metadata = reader.convertToMetadata();
+            System.out.println(metadata);
+            searchComponents(metadata);
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+
+    }
+    public static void searchComponents(Metadata metadata){
+        while (true){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter component name(-1 to exit):");
+            String componentName = in.nextLine();
+            if (componentName.equals("-1")) {
+                System.exit(0);
+            }
+            Component component = metadata.getComponent(componentName);
+            System.out.println(component);
+            for (Relation relation: metadata.getRelations()){
+                if (relation.source.equals(component) || relation.target.equals(component)){
+                    System.out.println(relation);
+                }
+            }
         }
     }
 }
